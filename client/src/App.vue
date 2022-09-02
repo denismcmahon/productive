cd95a22b05d75a4b7ba57fcaaacdc8302a5124aa
Denis McMahon <denismcmahon@gmail.com>
2022-09-02 16:00:54 +0100

    Frontend started, NavBar component added

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <NavBar />
+    <!--<router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>-->
+  </nav>
+  <router-view />
+</template>
+<script>
+import NavBar from './components/NavBar.vue';
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+<style></style>
