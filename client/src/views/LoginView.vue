463a66f462c846066e43158d319794b9b566594e
Denis McMahon <denismcmahon@gmail.com>
2022-09-13 16:32:22 +0100

    New production version ready

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -5,8 +5,6 @@
       <div class="card mx-auto">
         <div class="card-header text-white bg-primary">
           <h4>Login</h4>
-          <div>Name: {{ name_var }}</div>
-          <div>City: {{ city_var }}</div>
         </div>
         <div class="card-body">
           <form @submit.prevent="loginUser">
@@ -47,8 +45,6 @@ import { mapActions } from 'vuex';
 export default {
   data() {
     return {
-      name_var: process.env.VUE_APP_NAME_VAR,
-      city_var: process.env.VUE_APP_CITY_VAR,
       username: '',
       password: '',
     };
