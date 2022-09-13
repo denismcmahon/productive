575ef50581ed3ab8a0c6090036bc6ee1a8945e76
Denis McMahon <denismcmahon@gmail.com>
2022-09-13 10:43:11 +0100

    Production Database added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,12 @@
-module.exports = {
-  mongoURI: 'mongodb://localhost:27017/productive',
-  secret: 'messi>ronaldo',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mongoURI:
+      'mongodb+srv://denis:Adele260122@cluster0.zjswf.mongodb.net/productive?retryWrites=true&w=majority',
+    secret: 'messi>ronaldo',
+  };
+} else {
+  module.exports = {
+    mongoURI: 'mongodb://localhost:27017/productive',
+    secret: 'messi>ronaldo',
+  };
+}
